44903281299b5f8ca88aaf506e5e245d9cd3be7f
Martin Tran <martin.hua.tran@gmail.com>
2021-03-28 14:40:09 -0600

    Update docstring for Kruskal's with example usage.

--- graph_algorithms/minimum_spanning_tree.py
+++ graph_algorithms/minimum_spanning_tree.py
@@ -19,6 +19,16 @@ def kruskals(graph: Tuple[Number, Number, Number],
     Returns:
       The tuple of the subgraph which is an MST of |graph| in adjacency list
       form and the total cost of the MST.
+
+    Example graph from https://en.wikipedia.org/wiki/Minimum_spanning_tree#/media/File:Msp-the-cut-correct.svg
+
+    Usage: kruskals(g, 6)
+
+    g = [(0, 1, 1), (1, 0, 1), (0, 3, 3), (3, 0, 3),
+         (1, 2, 6), (2, 1, 6), (1, 3, 5), (3, 1, 5), (1, 4, 1), (4, 1, 1),
+         (2, 4, 5), (4, 2, 5), (2, 5, 2), (5, 2, 2),
+         (3, 4, 1), (4, 3, 1),
+         (4, 5, 4), (5, 4, 4)]
     """
     graph.sort(key=lambda edge: edge[2])
     forest = data_structures.UnionFind(n)
@@ -34,14 +44,6 @@ def kruskals(graph: Tuple[Number, Number, Number],
 
     return (mst, cost)
 
-"""
-Example graph from https://en.wikipedia.org/wiki/Minimum_spanning_tree#/media/File:Msp-the-cut-correct.svg
-
-Usage: kruskals(g, 6)
-
-g = [(0, 1, 1), (1, 0, 1), (0, 3, 3), (3, 0, 3),
-     (1, 2, 6), (2, 1, 6), (1, 3, 5), (3, 1, 5), (1, 4, 1), (4, 1, 1),
-     (2, 4, 5), (4, 2, 5), (2, 5, 2), (5, 2, 2),
-     (3, 4, 1), (4, 3, 1),
-     (4, 5, 4), (5, 4, 4)]
-"""
+def prims(graph: Tuple[Number, Number, Number],
+          n: int) -> Tuple[Dict[int, List[Tuple[int, int]]], ]:
+    pass  #TODO: Implement this.
